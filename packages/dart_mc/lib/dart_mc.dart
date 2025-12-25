30a64e92b2b646770bcdbb17327065aa2b4a131b
Norbert515 <kozsir.norbert@gmail.com>
2025-12-25 20:26:40 +0100

    Fix broken project templates for redstone create command
    - Fix mod template: use Events.addTickListener() instead of non-existent Events.onTick()
    - Fix mod_empty template: use Bridge.initialize() instead of non-existent initializeMod()
    - Update dart_mc library docstring to use correct API method name
    
    Projects created with `redstone create` now run successfully with `redstone run`.

--- packages/dart_mc/lib/dart_mc.dart
+++ packages/dart_mc/lib/dart_mc.dart
@@ -28,7 +28,7 @@
 ///   EntityRegistry.freeze();
 ///
 ///   // Register event handlers
-///   Events.onTick((tick) {
+///   Events.addTickListener((tick) {
 ///     // Called 20 times per second
 ///   });
 /// }
